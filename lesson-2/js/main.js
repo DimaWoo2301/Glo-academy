3a3df0bd1c8ead3b08f6b8dce510bae564accee0
DimaWoo2301 <DimaWoo2301@gmail.com>
2022-01-15 18:14:59 +0300

    Create: reviews

--- lesson-2/js/main.js
+++ lesson-2/js/main.js
@@ -1,10 +1,29 @@
-const swiper = new Swiper(".swiper", {
+const hotelSlider = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
+    nextEl: ".hotel-slider__button-next",
+    prevEl: ".hotel-slider__button-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider__button-next",
+    prevEl: ".slider__button-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
   },
 });
