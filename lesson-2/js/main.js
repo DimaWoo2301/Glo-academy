796eaf6ee1393b9261e94b0a408837eadc050caf
DimaWoo2301 <DimaWoo2301@gmail.com>
2022-01-16 05:48:28 +0300

    add: jquery file

--- lesson-2/js/main.js
+++ lesson-2/js/main.js
@@ -34,3 +34,22 @@ mebuButton.addEventListener("click", function () {
   document.querySelector(".navbar-mobile").toggleAttribute("hidden");
   document.querySelector(".navbar-bottom").toggleAttribute("hidden");
 });
+
+var modalButton = $('[data-toggle="modal" ]');
+var closeModalButton = $(".modal__close");
+
+modelButton.on("click", openModal);
+closeModalButton.on("click,closeModal");
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__Dialog");
+  modalOverlay.addClass(".modal__overlay--visible");
+  modalDialog.addClass(".modal__dialog--visible");
+}
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__Dialog");
+  modalOverlay.removeClass(".modal__overlay--visible");
+  modalDialog.removeClass(".modal__dialog--visible");
+}
